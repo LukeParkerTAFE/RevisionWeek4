657a48f334a68ae42e3f4011d66cda6688af9d28
Luke Parker <luke.parker9@tafensw.edu.au>
2020-06-12 14:13:54 +1000

    Updates to exercises

--- BookBorrowers.js
+++ BookBorrowers.js
@@ -1,3 +1,8 @@
+// const genre = Object.freeze({
+//     Fantasy: "Fantasy",
+//     Comedy: "Comedy"
+// });
+
 // List of all people who can borrow books
 let borrowers = [
     {
@@ -17,17 +22,27 @@ let books = [
     {
         id: 1,
         title: "Harry Potter and the Philosopher's Stone",
-        genre: "Fantasy"
+        genreId: 1
     }, {
         id: 2,
         title: "Harry Potter and the Chamber of Secrets",
-        genre: "Fantasy"
+        genreId: 1
     }, {
         id: 3,
         title: "The Hitchhiker's Guide to the Galaxy",
-        genre: "Comedy"
+        genreId: 2
     }
 ];
+
+let genres = [
+    {
+        id: 1,
+        name: "Fantasy"
+    }, {
+        id: 2,
+        name: "Comedy"
+    }
+]
 
 // List of all books currently being borrowed
 let bookBorrowers = [
@@ -43,6 +58,12 @@ let bookBorrowers = [
     }, {
         borrowerId: 3,
         bookId: 2
+    }, {
+        borrowerId: 2,
+        bookId: 3
+    }, {
+        borrowerId: 3,
+        bookId: 3
     }
 ];
 
@@ -50,13 +71,34 @@ function getBookById(bookId) {
     return books.find(book => book.id == bookId);
 }
 // console.log(getBookById(3));
+function getBorrowerById(borrowerId) {
+    return borrowers.find(borrower => borrower.id == borrowerId);
+}
+
+function getGenreById(genreId) {
+    return genres.find(genre => genre.id == genreId);
+}
 
 function getBooksByPerson(borrowerId) {
     let booksBorrowedByPerson = bookBorrowers.filter(bb => bb.borrowerId == borrowerId);
-    // return booksBorrowedByPerson;
     let bookIds = booksBorrowedByPerson.map(bb => bb.bookId);
-    // return bookIds;
     return bookIds.map(id => getBookById(id));
 }
+// console.log(getBooksByPerson(1));
 
-console.log(getBooksByPerson(1));
+function getPeoplebyBook(bookId) {
+    let peopleWhoHaveBorrowed = bookBorrowers.filter(bb => bb.bookId == bookId);
+    let borrowerIds = peopleWhoHaveBorrowed.map(bb => bb.borrowerId);
+    return borrowerIds.map(id => getBorrowerById(id));
+}
+
+// console.log(getPeoplebyBook(2));
+
+function getGenresBorrowedByPerson(borrowerId) {
+    let booksBorrowed = getBooksByPerson(borrowerId);
+    let genreIds = booksBorrowed.map(book => book.genreId);
+    let genres = genreIds.map(id => getGenreById(id));
+    return genres.map(genre => genre.name);
+}
+
+console.log(getGenresBorrowedByPerson(3));
